5f70f43b952055f3bb2179f15716310bff3e8a23
patry <patryk.gajewski@pollub.edu.pl>
2022-10-13 12:44:57 +0200

    currency table added

---
+++ database/migrations/2022_10_13_103009_create_currency_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currency', function (Blueprint $table) {
+            $table->uuid();
+            $table->string('name',60)->unique();
+            $table->string('currency_code',10);
+            $table->decimal('exchange_rate',19,9);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currency');
+    }
+};
